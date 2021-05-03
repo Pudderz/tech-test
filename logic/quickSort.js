e16e83c4099b18279b598e1bbaee5ce0fac9bc27
Pudderz <mpudney6@gmail.com>
2021-05-03 21:31:00 +0100

    Added quickSort with test
    for sorting an array after the fetching has stopped to work out the outliers

---
+++ logic/quickSort.js
@@ -0,0 +1,22 @@
+function quickSort(array) {
+    if (array.length <= 1) return array;
+    const pivot = array.pop();
+    const lessThanContainer = [];
+    const greaterThanContainer = [];
+  
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] <= pivot) {
+        lessThanContainer.push(array[i]);
+      } else {
+        greaterThanContainer.push(array[i]);
+      }
+    }
+  
+    return [
+      ...quickSort(lessThanContainer),
+      pivot,
+      ...quickSort(greaterThanContainer),
+    ];
+  }
+
+module.exports = {quickSort};
---
+++ logic/quickSort.test.js
@@ -0,0 +1,13 @@
+const { quickSort } = require("./quickSort");
+
+test("test any number of array items", () => {
+  expect(quickSort([0, 8, 3, 4, 12])).toEqual([0, 3, 4, 8, 12]);
+});
+
+test("no array items", () => {
+  expect(quickSort([])).toEqual([]);
+});
+
+test("test one array items", () => {
+  expect(quickSort([1])).toEqual([1]);
+});
