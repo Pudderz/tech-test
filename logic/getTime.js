b2c0582a34b850b98f75323490900793837b236c
Pudderz <mpudney6@gmail.com>
2021-05-03 21:30:11 +0100

    created getTimeDiffFromString function with tests
    converts start and endtime ISO strings dates into milliseconds

---
+++ logic/getTime.js
@@ -0,0 +1,27 @@
+const getTimeDiffFromString = ({ startTime, endTime } = {}) => {
+  if (typeof startTime !== "string" || typeof endTime !== "string") {
+    throw new Error(
+      "Invalid date input, startTime or endTime should be as ISO string"
+    );
+  }
+
+  const startDate = new Date(startTime);
+  const endDate = new Date(endTime);
+
+  if (isNaN(startDate) || isNaN(endDate))
+    throw new Error(
+      "Invalid date input, startTime or endTime should be as ISO string"
+    );
+
+  const difference = endDate.getTime() - startDate.getTime();
+
+  if (difference < 0) {
+    throw new Error("Invalid time, start date is after end date.");
+  }
+
+  // not converting to minutes here to reduce rounding issues
+  // when doing the maths on decimals (stays as milliseconds)
+  return difference;
+};
+
+module.exports = { getTimeDiffFromString };
---
+++ logic/getTime.test.js
@@ -0,0 +1,54 @@
+const { getTimeDiffFromString } = require("./getTime");
+
+test("Convert startDate and endDate to difference", () => {
+  expect(
+    getTimeDiffFromString({
+      startTime: "2021-05-03T19:07:28.213Z",
+      endTime: "2021-05-03T19:17:28.213Z",
+    })
+  ).toBe(600000);
+});
+
+test("Test no time input to throw error", () => {
+  expect(() => getTimeDiffFromString()).toThrow(
+    "Invalid date input, startTime or endTime should be as ISO string"
+  );
+});
+
+test("Empty strings to throw error", () => {
+  expect(() =>
+    getTimeDiffFromString({
+      startTime: "",
+      endTime: "",
+    })
+  ).toThrow(
+    "Invalid date input, startTime or endTime should be as ISO string"
+  );
+});
+
+test("any nonTimeFormat strings to throw error", () => {
+  expect(() =>
+    getTimeDiffFromString({
+      startTime: "any input",
+      endTime: "any input as well",
+    })
+  ).toThrow("Invalid date input, startTime or endTime should be as ISO string");
+});
+
+test("endDate that are smaller than startDate will throw error", () => {
+  expect(() =>
+    getTimeDiffFromString({
+      startTime: "2021-06-02T18:06:28.213Z",
+      endTime: "2021-05-03T19:07:28.213Z",
+    })
+  ).toThrowError("Invalid time, start date is after end date.");
+});
+
+test("test change of date (overnight)", () => {
+    expect(
+    getTimeDiffFromString({
+      startTime: "2021-05-02T23:06:28.213Z",
+      endTime: "2021-06-03T01:07:28.213Z",
+    })
+  ).toBe(2685660000);
+});
