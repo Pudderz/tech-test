976aed833e6aa4a4ae8b5707278e3c91b0f22ad3
Pudderz <mpudney6@gmail.com>
2021-05-04 12:54:48 +0100

    added function to display average results

---
+++ logic/displayValues.js
@@ -0,0 +1,41 @@
+const displayValues = (survey, totalSurveysDone) => {
+  const {
+    averageTime,
+    allTimes,
+    lowerQuartile,
+    upperQuartile,
+    surveyId,
+  } = survey;
+
+  console.log(
+    "\n \x1b[33m%s\x1b[0m",
+    `Survey ID - ${surveyId} \n Average time - ${Math.round(
+      averageTime / 1000 / 60
+    )} minutes`
+  );
+  if (allTimes) {
+    console.log(
+      `\n min time - ${Math.round(allTimes[0] / 1000 / 60)} minutes ${
+        Math.round(allTimes[0] / 1000) % 60
+      } seconds`,
+      `\n max time - ${Math.round(
+        allTimes[allTimes.length - 1] / 1000 / 60
+      )} minutes ${
+        Math.round(allTimes[allTimes.length - 1] / 1000) % 60
+      } seconds`
+    );
+  }
+
+  if (lowerQuartile) {
+    console.log(
+      ` IQR - ${Math.round(lowerQuartile / 1000 / 60)} to ${Math.round(
+        upperQuartile / 1000 / 60
+      )} minutes`
+    );
+  }
+  console.log(
+    ` Percentage received - ${((allTimes.length / totalSurveysDone) * 100).toFixed(2)}%`
+  );
+};
+
+module.exports = { displayValues };
