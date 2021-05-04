996ba7dca0f1a3e7845187500504459033cefec6
Pudderz <mpudney6@gmail.com>
2021-05-04 12:57:18 +0100

    created index.js
    creates a new class and queries it in a while loop till iterator is over MAX_REQUEST_LIMIT , survey is complete or survey averages are checked and done.
    
    once they are complete it sorts the survey times, removes outliers and displays the data in the console

---
+++ index.js
@@ -0,0 +1,50 @@
+const { GetSurveys } = require("./classes/GetSurveys");
+const { performance } = require("perf_hooks");
+const {
+  INITIAL_NUMBER_OF_REQUESTS,
+  MAX_REQUEST_LIMIT,
+  DECREASE_BY,
+  API_BACKEND,
+} = require("./globals");
+
+
+
+const workOutNumOfRequests = (numberOfRequests) => {
+  return numberOfRequests / DECREASE_BY > 5
+    ? Math.floor(numberOfRequests / DECREASE_BY)
+    : 5;
+};
+
+
+const averageSurveys = new GetSurveys(API_BACKEND);
+
+//loops till iterator(i) is over MAX_REQUEST_LIMIT, fetchBulkData encounters
+// a network error or surveys averages are done
+
+const findAverage = (async () => {
+  const startTime = performance.now();
+  let numberOfRequests = INITIAL_NUMBER_OF_REQUESTS;
+  let shouldStop = false;
+  let i = 0;
+
+  while (i < MAX_REQUEST_LIMIT && !shouldStop) {
+
+    ({shouldStop} = await averageSurveys.fetchBulkNum(numberOfRequests));
+    if(shouldStop) break;
+
+    shouldStop = averageSurveys.checkIsDone();
+    console.log(numberOfRequests);
+    i = numberOfRequests + i;
+
+    numberOfRequests = workOutNumOfRequests(numberOfRequests);
+  }
+
+  console.log(`${i} times ran`);
+
+  averageSurveys.sortSurveyResults().removeAllOutliers();
+
+  averageSurveys.displayData();
+  console.log(performance.now() - startTime);
+})();
+
+
