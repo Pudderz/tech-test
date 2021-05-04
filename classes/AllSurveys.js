3ed3c2fcf2bfb69ad16bbb06f70c46a51aec7184
Pudderz <mpudney6@gmail.com>
2021-05-04 11:52:15 +0100

    Added AllSurvey class code and tests

--- classes/AllSurveys.js
+++ classes/AllSurveys.js
@@ -1,23 +1,61 @@
+const Survey = require("./Survey");
 
 class AllSurveys {
   constructor() {
-    this._surveys = {};
+    this.surveys = {};
     this.numOfSurveys = 0;
     this.numOfSurveyAveragesDone = 0;
     this.totalUserSurveys = 0;
   }
 
-  addSurvey() {}
+  checkAndAddSurvey(surveyId) {
+    if (typeof this.surveys[surveyId] == "undefined") {
+      this.surveys[surveyId] = new Survey(surveyId);
+      this.numOfSurveys += 1;
+    }
+  }
 
-  checkIsDone() {}
+  checkIsDone() {
+    return this.numOfSurveyAveragesDone == this.numOfSurveys;
+  }
 
-  addData() {}
+  addData({ surveyId, time }) {
+    this.checkAndAddSurvey(surveyId);
+    this.totalUserSurveys++;
 
-  sortSurveyResults() {}
+    const survey = this.surveys[surveyId];
+    survey.addTime(time);
 
-  removeAllOutliers() {}
+    if (survey.checkIfIsDoneChanged()) {
+      this.numOfSurveyAveragesDone += survey.isDone ? 1 : -1;
+    }
 
-  displayData() {}
+    return this;
+  }
+
+  sortSurveyResults() {
+    for (const surveyId in this.surveys) {
+      this.surveys[surveyId].sortAllTimes();
+    }
+    return this;
+  }
+
+  removeAllOutliers() {
+    for (const surveyId in this.surveys) {
+      const totalSurveysRemoved = this.surveys[surveyId].removeOutliers();
+      this.totalUserSurveys -= totalSurveysRemoved;
+    }
+    return this;
+  }
+
+  displayData() {
+    for (const surveyId in this.surveys) {
+      if (this.surveys[surveyId] instanceof Survey) {
+        this.surveys[surveyId].displaySurveyData(this.totalUserSurveys);
+      }
+    }
+    return this;
+  }
 }
 
-module.exports =  AllSurveys;
+module.exports = AllSurveys;
